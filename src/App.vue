6a710dd54c1d176083a26f2731dba0cc0630c025
Christiaan Quyn <christianq010@gmail.com>
2018-03-31 02:46:31 +0530

    Add a local directive

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
                 <h1 v-highlight:background.delayed="'red'">Directives</h1>
                 <h3 v-highlight="'red'">Random text</h3>
                 <p v-text="'Some text'"></p>
+                <h1 v-local-highlight:background.delayed="'red'">Local Directive</h1>
             </div>
         </div>
     </div>
@@ -12,6 +13,24 @@
 
 <script>
     export default {
+        directives: {
+            'local-highlight': {
+                bind(el, binding, vnode) {
+                    // Add delay of 3 seconds before style is applied
+                    var delay = 0;
+                    if (binding.modifiers['delayed']) {
+                    delay = 3000;
+                    }
+                    setTimeout(() => {
+                    if (binding.arg == 'background') {
+                        el.style.backgroundColor = binding.value;
+                    } else {
+                        el.style.color = binding.value;
+                    }
+                    }, delay);
+                }
+            }
+        }
     }
 </script>
 
