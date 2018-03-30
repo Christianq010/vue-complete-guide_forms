cfdfb76964789430917ef14b29e803c1c15263bd
Christiaan Quyn <christianq010@gmail.com>
2018-03-31 02:40:05 +0530

    Modify a custom directive

--- src/main.js
+++ src/main.js
@@ -5,11 +5,18 @@ Vue.directive('highlight', {
   bind(el, binding, vnode){
     // el.style.backgroundColor = 'green';
     // el.style.backgroundColor = binding.value;
-    if (binding.arg == 'background') {
-      el.style.backgroundColor = binding.value;
-    } else {
-      el.style.color = binding.value;
+    // Add delay of 3 seconds before style is applied
+    var delay = 0;
+    if (binding.modifiers['delayed']) {
+      delay = 3000;
     }
+    setTimeout(() => {
+      if (binding.arg == 'background') {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay);
   }
 });
 
