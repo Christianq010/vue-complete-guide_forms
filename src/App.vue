54613ff648ae722ba17c6e299f194ee538da35e1
Christiaan Quyn <christianq010@gmail.com>
2018-04-03 22:51:06 +0530

    Setting up for transitions

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
-
+                <hr>
+                <button class="btn btn-primary" @click="show= !show">Some Button</button>
+                <br>
+                <br>
+                <div class="alert-success text-center" v-if="show">This is an Infowindow</div>
             </div>
         </div>
     </div>
@@ -13,7 +17,7 @@
     export default {
         data() {
             return {
-
+                show: false
             }
         }
     }
