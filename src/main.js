2f6e4e082c470ce1dd2df9d6f47a35adc3cb5525
Christiaan Quyn <christianq010@gmail.com>
2018-04-10 20:44:11 +0530

    Intercepting Requesting , Intercepting Responses

--- src/main.js
+++ src/main.js
@@ -5,6 +5,16 @@ import App from './App.vue'
 Vue.use(VueResource);
 
 Vue.http.options.root = 'https://vuejs-project-ff0eb.firebaseio.com/data.json';
+// Functions on what we want execute on each request
+Vue.http.interceptors.push((request, next) =>{
+  console.log(request);
+  if (request.method == 'POST') {
+    request.method = 'PUT';
+  }
+  next(response => {
+    response.json = () => {return {messages: response.body}}
+  });
+});
 
 new Vue({
   el: '#app',
