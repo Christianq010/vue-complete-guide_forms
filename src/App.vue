5ed6f5eee6031c9b84eaca14653b314af9a13997
Christiaan Quyn <christianq010@gmail.com>
2018-04-10 19:38:46 +0530

    Creating an App and set up Firebase, and POSTing a data to that server successfully

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Http</h1>
+                <div class="form-group">
+                    <label for="">Username</label>
+                    <input class="form-control" type="text" v-model="user.username">
+                </div>
+                <div class="form-group">
+                    <label for="">Email</label>
+                    <input class="form-control" type="text" v-model="user.email">
+                </div>
+                <button class="btn btn-primary" v-on:click="submit">Submit</button>
             </div>
         </div>
     </div>
@@ -10,6 +19,25 @@
 
 <script>
     export default {
+        data() {
+            return {
+                user: {
+                    username: '',
+                    email: ''
+                }
+            }
+        },
+        methods: {
+            submit() {
+                this.$http.post('https://vuejs-project-ff0eb.firebaseio.com/data.json', this.user)
+                    .then(response => {
+                        console.log(response);
+                    }, error => {
+                        console.log(error);
+                    });
+                // console.log(this.user);
+            }
+        }
     }
 </script>
 
