760b8e83ddb60801ffe920b5966a32d358d5450a
Christiaan Quyn <christianq010@gmail.com>
2018-04-18 17:45:21 +0530

    Route Parameters (Fetching - reacting to changes in route parameters)

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -2,11 +2,22 @@
     <div>
         <h1>The User Page</h1>
         <hr>
+        <h3>Loaded ID: {{id}} </h3>
         <button @click="navgateToHome" class="btn btn-info">Go to Home</button>
     </div>
 </template>
 <script>
     export default {
+        data() {
+            return {
+                id: this.$route.params.id
+            }
+        },
+        watch: {
+            '$route'(to, from) {
+                this.id = to.params.id;
+            }
+        },
         methods: {
             navgateToHome() {
                 this.$router.push('/');
